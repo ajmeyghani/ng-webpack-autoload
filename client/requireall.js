b8ea2d862da854f3a947945e1f7feb06c96eb92c
Amin Meyghani <meyghania@gmail.com>
2016-09-08 17:47:59 -0700

    remove server and extra files.

--- client/requireall.js
+++
@@ -1,7 +0,0 @@
-/*
- * Helper to get the list of the files.
- * specific to Webpack.
-*/
-export default function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-}
