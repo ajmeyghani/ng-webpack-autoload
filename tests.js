7187310ae0997a4765a8717e1ba52c147b25d49d
Amin Meyghani <meyghania@gmail.com>
2016-09-09 17:45:24 -0700

    add the test configuration

--- tests.js
+++ tests.js
@@ -1,2 +1,7 @@
-const testContext = require.context('./src', true, /\.test\.js$/);
-testContext.keys().map(testContext);
+// import angular from 'angular';
+
+// const testContext = require.context('./src', true, /\.test\.js$/);
+// testContext.keys().map(testContext);
+
+window.expect = chai.expect;
+window.helper = bard;
